23d21d273ec16b753bc970f0d9640d8f51a41d3a
Henrique dos Santos <henriquesqs@hotmail.com>
2020-04-10 16:05:10 -0300

    Fixed memory leaks

--- Images Compressor/compressor.c
+++ Images Compressor/compressor.c
@@ -3,10 +3,21 @@
 int main(int argc, char const *argv[])
 {
     FILE *file;
-    BMPINFOHEADER * bmpInfo;
-    BMPFILEHEADER * bmpFile;
 
-    file = fopen("images/1000x624.bmp", "r");
+    BMPFILEHEADER *bmpFile = (BMPFILEHEADER*) malloc(sizeof(bmpFile));
+    BMPINFOHEADER *bmpInfo = (BMPINFOHEADER*) malloc(sizeof(bmpInfo));
+    ;
 
-    if(file == NULL) printf("error");
+    file = fopen("images/1000x624.bmp", "r"); // opens the image that we want to compress
+
+    if(file == NULL) printf("error"); // check if there was an error opening the image
+
+    readBMPFileHeader(file, bmpFile);
+    readBMPInfoHeader(file, bmpInfo);
+
+    free(bmpFile);
+    free(bmpInfo);
+    fclose(file);
+
+    return 1;
 }
